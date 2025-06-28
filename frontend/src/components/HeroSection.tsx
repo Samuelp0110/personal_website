af249e9d050993f7ccfb63c6014ec69512a2053a
Samuelp0110 <spreston110@gmail.com>
2025-06-28 19:33:31 -0400

    Updated Profile Pic

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,23 +1,23 @@
 import type { FC } from "react";
-import ProfileImage from "../assets/sam-preston.jpg";
+import ProfileImage from "../assets/SP_GradPhoto.jpg";
 
 const HeroSection: FC = () => {
   return (
-    <section className='w-full px-6 py-24 flex flex-wrap justify-center items-center gap-10'>
-      <div className='max-w-[1000px] w-full flex flex-col md:flex-row items-center gap-6'>
+    <section className="w-full px-6 py-24 flex flex-wrap justify-center items-center gap-10">
+      <div className="max-w-[1000px] w-full flex flex-col md:flex-row items-center gap-6">
         {/* Profile Image */}
         <img
           src={ProfileImage}
-          alt='Sam Preston'
-          className='w-[200px] h-[200px] rounded-full border border-background object-cover'
+          alt="Sam Preston"
+          className="w-[200px] h-[200px] rounded-full border border-background object-cover"
         />
 
         {/* Text Block */}
-        <div className='flex-1 flex flex-col gap-4 text-left font-cormorant text-background'>
-          <h1 className='text-[36px] md:text-[44px] font-bold'>
+        <div className="flex-1 flex flex-col gap-4 text-left font-cormorant text-background">
+          <h1 className="text-[36px] md:text-[44px] font-bold">
             Hello, I’m Sam Preston
           </h1>
-          <p className='text-[28px] md:text-[32px] font-semibold leading-snug'>
+          <p className="text-[28px] md:text-[32px] font-semibold leading-snug">
             With a creative personality and an inquisitive passion, I find
             myself drawn to almost anything involving a story. And everything
             has a story, from data to people.
